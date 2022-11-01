8bf7aaaf9f7459b9647fb7a8db9bfb7fa101adf1
f <lol@gmail.com>
2022-11-01 23:42:05 +0300

    помогли

---
+++ db_logic.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine, inspect, select
+from sqlalchemy import Table, Column, Integer, String, MetaData, Date, Numeric, Boolean, Time
+
+DBFILEPATH = 'sqlite:///db.db'
+
+engine = create_engine(DBFILEPATH, echo=True)
+meta = MetaData()
+
+tests_results = Table(
+    'tests_results', meta,
+    Column('counter', Integer, primary_key=True),
+    Column('user', String),
+    Column('time_current', Integer),
+    Column('type_action', String),
+    Column('source_index', Integer),
+    Column('destination_index', Integer),
+    sqlite_autoincrement=True
+)
+
+meta.create_all(engine)
+
+def record_users_action(user,time_current, type_action, source_index, destination_index):
+    with engine.connect() as conn:
+        stmt1 = tests_results.insert().values(user=user, time_current=time_current, type_action=type_action, source_index=source_index, destination_index=destination_index)
+        conn.execute(stmt1)
--- flask_server.py
+++ flask_server.py
@@ -1,86 +1,98 @@
 from flask import Flask, render_template, request, make_response, session, redirect
-# import db_logic
+import db_logic
 import os
 import json
 import re
 import uuid
-import datetime 
+import datetime
 
 server_object = Flask(__name__)
 
+
 @server_object.route('/index.html', methods=['GET', 'POST'])
 def index():
-    if request.method  == 'GET':
+    if request.method == 'GET':
         return render_template('index.html')
 
-@server_object.route('/start.html', methods=['GET', 'POST'])
+
+@server_object.route('/start.html', methods=['GET'])
 def init_test():
     def get_json_q():
         with open('questioons_base.json') as f:
             q_base = json.load(f)
         return q_base
-    
-    if request.method  == 'GET':
+
+    if request.method == 'GET':
         session['points'] = 0
-        session['user_name'] = uuid.uuid1()
+        session['user_name'] = str(uuid.uuid1())
         session['tasks'] = get_json_q()
         session['time_start'] = datetime.datetime.now(datetime.timezone.utc)
         session['time_last_q_started'] = datetime.datetime.now(datetime.timezone.utc)
         session['cur_section'] = 0
         session['is_trapped'] = False
-        
+
         return redirect('/next_question.html')
 
 
-
-@server_object.route('/next_question.html', methods=['GET', 'POST'])
+@server_object.route('/next_question.html', methods=['GET'])
 def show_question():
-    if request.method  == 'GET':
-        print('дай вопрос из секции номер', session['cur_section'])
-        if (session['time_last_q_started'] - session['time_start']).total_seconds() > 15*60 or session['tasks'][ session['cur_section']] == []:
-            return render_template('index.html')
+    print('#####', request.method)
+    print('ЗАПРОС на вопрос из секции', session['cur_section'])
+    if (session['time_last_q_started'] - session['time_start']).total_seconds() > 15*60 or session['tasks'][session['cur_section']] == []:
+        return render_template('index.html')
 
-        question = session['tasks'][session['cur_section']].pop(0)
-        session['time_last_q_started'] = datetime.datetime.now(datetime.timezone.utc)
-        print(question, session['cur_section'] )
-        return render_template('question_form.html', question=question)
-    if request.method  == 'POST':
-        print('пришел ответ для вопроса из секции номер', session['cur_section'])
-        answer = request.form['answer'] == "true"
-        print(session['is_trapped'], bool(answer), session['cur_section'])
-        if session['is_trapped']:
-            if  (datetime.datetime.now(datetime.timezone.utc) - session['time_last_q_started']).total_seconds() >= 32 or answer==False:
-                pass
-            else:
-                print('вышел из ловушки', session['cur_section'], datetime.datetime.now(datetime.timezone.utc) -session['time_start'] )
-                session['is_trapped'] = False
+    question = session['tasks'][session['cur_section']].pop(0)
+    print('Выдергнут вопрос:', question)
+
+    session['time_last_q_started'] = datetime.datetime.now(
+        datetime.timezone.utc)
+    return render_template('question_form.html', question=question)
+
+
+@server_object.route('/send_answer', methods=['POST'])
+def send_answer():
+    user, time_current, type_action, source_index, destination_index = session['user_name'], (datetime.datetime.now(datetime.timezone.utc) - session['time_last_q_started']).total_seconds(), None, session['cur_section'], None
+    print('#####', request.method)
+    print('пришел ответ для вопроса из секции номер', session['cur_section'])
+    answer = request.form['answer'] == "true"
+    print(session['is_trapped'], bool(answer), session['cur_section'])
+    if session['is_trapped']:
+        if time_current >= 32 or answer == False:
+            type_action = 'stay_in_trap'
         else:
-            if (datetime.datetime.now(datetime.timezone.utc) - session['time_last_q_started']).total_seconds() >= 32:
-                session['is_trapped'] = True
-                print('пошел в ловушку',  session['cur_section'], datetime.datetime.now(datetime.timezone.utc) -session['time_start'] )
-            elif answer == False:
-                session['points'] -= 1
-                if  session['points'] == -3:
-                    if session['cur_section'] > 0:
-                        print('понижение секции' ,session['cur_section'], "->",session['cur_section']-1,  datetime.datetime.now(datetime.timezone.utc) -session['time_start'] )
-                    session['cur_section'] = max(0, session['cur_section']-1)
-                    session['points'] = 0
-            elif answer == True:
-                if session['cur_section'] < 2:
-                    print('up секции' ,session['cur_section'], "->",session['cur_section']+1,  datetime.datetime.now(datetime.timezone.utc) -session['time_start'] )
-                session['cur_section'] = min(2, session['cur_section']+1)
+            type_action = 'from_trap'
+            print('вышел из ловушки', session['cur_section'], datetime.datetime.now(
+                datetime.timezone.utc) - session['time_start'])
+            session['is_trapped'] = False
+    else:
+        if (datetime.datetime.now(datetime.timezone.utc) - session['time_last_q_started']).total_seconds() >= 32:
+            session['is_trapped'] = True
+            type_action = 'to_trap'
+            print('пошел в ловушку',  session['cur_section'], datetime.datetime.now(datetime.timezone.utc) - session['time_start'])
+        elif answer == False:
+            session['points'] -= 1
+            type_action = 'stay'
+            if session['points'] == -3:
+                if session['cur_section'] > 0:
+                    type_action = 'down_section'
+                    print('понижение секции', session['cur_section'], "->", session['cur_section'] - 1,  datetime.datetime.now(datetime.timezone.utc) - session['time_start'])
+                session['cur_section'] = max(0, session['cur_section']-1)
                 session['points'] = 0
-        print(session['is_trapped'], answer, session['cur_section'])
-        return 'ПОМОГИТИ ПОЖАЛУЙСТА'
+        elif answer == True:
+            type_action = 'stay'
+            if session['cur_section'] < 2:
+                type_action = 'up_section'
+                print('up секции', session['cur_section'], "->", session['cur_section']+1, datetime.datetime.now(datetime.timezone.utc) - session['time_start'])
+            session['cur_section'] = min(2, session['cur_section']+1)
+            session['points'] = 0
+    print(session['is_trapped'], answer, session['cur_section'])
 
 
-
-
-
-
-
+    destination_index = session['cur_section']
+    current_action_info = db_logic.record_users_action(user,time_current, type_action, source_index, destination_index)
+    return 'ok'
 
 
 if __name__ == '__main__':
     server_object.secret_key = 'abc'
-    server_object.run(debug=False)
+    server_object.run(debug=True)
